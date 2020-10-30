b29a95382fde0e1a98c32290d9c98f6d6729f076
levg34 <levg34@users.noreply.github.com>
2020-10-30 18:10:39 +0100

    ameliorate front

--- js/test.js
+++ js/test.js
@@ -18,3 +18,15 @@ let testChange2 = new Change(null,true)
 testDay.addChange(testChange2)
 console.log(testDay)
 console.log(testDay.didPoo())
+
+let testVomit1 = new Vomit('02:56')
+let testVomit2 = new Vomit('15:33','blue colour')
+testDay.addVomit(testVomit1)
+testDay.addVomit(testVomit2)
+
+let testBath = new Bath('23:56')
+testDay.setBath(testBath)
+let testVitamin = new Vitamin('08:55',3)
+testDay.setVitamin(testVitamin)
+
+console.log(testDay)
