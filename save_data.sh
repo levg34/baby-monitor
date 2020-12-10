3f2eef99238651e9c30ff1c29acf966a7fd93e38
levg34 <levg34@users.noreply.github.com>
2020-12-10 21:27:23 +0100

    get prod data in dev

--- save_data.sh
+++ save_data.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp ~/prod/baby-monitor/data/datafile ~/prod/backup/datafile
+cp ~/prod/baby-monitor/data/datafile ./data/datafile
 cp ~/prod/baby-monitor/data/datafile /mnt/usb/backup/datafile
