586d040251a92bf5c24bae47906b73dcd94f6732
levg34 <guilhem.dross@laposte.net>
2020-12-16 22:42:49 +0100

    add useful functions to timeUtils.js

--- public/js/timeUtils.js
+++ public/js/timeUtils.js
@@ -1,7 +1,6 @@
 class TimeUtils {
     static today() {
-        let dateString = new Date().toISOString()
-        return dateString.slice(0,dateString.indexOf('T'))
+        return this.jsDateToDateString(new Date())
     }
 
     static now() {
@@ -11,10 +10,33 @@ class TimeUtils {
         return hours+':'+minutes
     }
 
+    static jsDateToDateString(date) {
+        let dateString = date.toISOString()
+        return dateString.slice(0,dateString.indexOf('T'))
+    }
+
     static localeDate(date,locale) {
         if (!date) {
             date = this.today()
         }
         return new Intl.DateTimeFormat(locale).format(new Date(date))
+    }
+
+    static nDaysFrom(date,days) {
+        var oldDay = date ? new Date(date) : new Date()
+        oldDay.setDate(oldDay.getDate() - days)
+        return this.jsDateToDateString(oldDay)
+    }
+
+    static oneWeekFrom(date) {
+        return this.nDaysFrom(date,7)
+    }
+
+    static dayBefore(date) {
+        return this.nDaysFrom(date,1)
+    }
+
+    static oneWeekInterval(date) {
+        return [this.oneWeekFrom(date),this.dayBefore(date)]
     }
 }
