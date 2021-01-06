5fe654611d4c567795e18b7f26b20cea863f9409
levg34 <guilhem.dross@laposte.net>
2021-01-06 20:07:15 +0100

    create one save per day

--- save_data.sh
+++ save_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cp ~/prod/baby-monitor/data/datafile ~/prod/backup/datafile
+date=`date +%Y-%d-%m`
+cp ~/prod/baby-monitor/data/datafile ~/prod/backup/${date}-datafile
 cp ~/prod/baby-monitor/data/datafile ./data/datafile
-cp ~/prod/baby-monitor/data/datafile /mnt/usb/backup/datafile
+cp ~/prod/baby-monitor/data/datafile /mnt/usb/backup/${date}-datafile
