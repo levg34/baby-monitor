01a637270086857681f9fcc99801004755d18e89
levg34 <levg34@users.noreply.github.com>
2021-01-29 19:11:26 +0100

    fix save_data script

--- save_data.sh
+++ save_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-date=`date +%Y-%d-%m`
+date=`date +%Y-%m-%d`
 cp ~/prod/baby-monitor/data/datafile ~/prod/backup/${date}-datafile
 cp ~/prod/baby-monitor/data/datafile ./data/datafile
 cp ~/prod/baby-monitor/data/datafile /mnt/usb/backup/${date}-datafile
